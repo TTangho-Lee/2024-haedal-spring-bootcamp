ff4b74bc5435eedcece692870a20fd5361525e11
SeungHo <seungho4136@naver.com>
2024-09-25 19:20:19 +0900

    week4

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.requestdto.UserRegistrationRequestDto;
+import com.example.demo.service.UserService;
+import com.example.demo.dto.responsedto.UserSimpleResponseDto;
+import com.example.demo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<UserSimpleResponseDto> registerUser(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto) {
+        User user = new User(
+                userRegistrationRequestDto.getUsername(),
+                userRegistrationRequestDto.getPassword(),
+                userRegistrationRequestDto.getName()
+        );
+        UserSimpleResponseDto savedUser = userService.saveUser(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
+}
